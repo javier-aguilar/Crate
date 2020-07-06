ada33f03cd873dd7eb12c035e0db9d2ceb26c9ef
Charlie Bandstra <bandstracharlie@gmail.com>
2020-07-05 21:40:50 -0600

    Add app annotations FE

--- code/web/src/setup/client/App.js
+++ code/web/src/setup/client/App.js
@@ -8,6 +8,8 @@ import Layout from '../../modules/common/Layout'
 import NotFound from '../../modules/common/NotFound'
 import RoutePrivate from '../../modules/auth/RoutePrivate'
 
+//The app is rendering compoenents based on routes from the routes directory. I am struggling to understand why the route.path would ever be a function. If I can figure out where the compoenents are actually being rendered then I will be able to add new components with ease. 
+
 const App = () => (
   <Layout>
     <Switch>
