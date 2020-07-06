7907588d81cc37199b084e01369ad05ae95796c5
Charlie Bandstra <bandstracharlie@gmail.com>
2020-07-06 16:06:02 -0600

    Add baseline editprofile page

--- code/web/src/modules/user/EditProfile.js
+++ code/web/src/modules/user/EditProfile.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { updateUser } from './api/actions'
 
-export default class EditProfile extends Component {
+class EditProfile extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        error: '',
+        isLoading: false,
+        user: {
+          name: this.props.user.details.name,
+          email: this.props.user.details.email,
+          password: this.props.user.details.password,
+        }
+      }
+  }
+
+  onChange = (event) => {
+    let user = this.state.user
+    user[event.target.name] = event.target.value;
+
+    this.setState({user});
+  }
+
+  onClick = (event) => {
+    updateUser(this.state.user)
+  }
+
+
   render() {
     return (
       <div>
-        
-      </div>
+      <input name="email" onChange={this.onChange} value={this.state.user.email} />
+      <button onClick={this.onClick}>Save</button>
+    </div>
     )
   }
 }
+
+const editProfileState = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(editProfileState)(EditProfile)
