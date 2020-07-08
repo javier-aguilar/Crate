8d492feb5bf1fbe4e96a90ac65b693f4d6448fc1
Mike Hernandez <hernandezmichael321@gmail.com>
2020-07-07 19:20:39 -0600

    Create tests for user update endpoint

--- code/api/src/modules/user/query.test.js
+++ code/api/src/modules/user/query.test.js
@@ -4,8 +4,9 @@ import graphqlHTTP from 'express-graphql'
 import schema from '../../setup/schema';
 
 describe("user queries", () => {
+  const server =  express();
+
   beforeAll(()  => {
-    const server = express();
     server.use(
       '/',
       graphqlHTTP({
@@ -17,5 +18,49 @@ describe("user queries", () => {
 
   it("is true", () => {
     expect(true).toBe(true)
+  })
+
+  it("can update a user's email address", async () => {
+    const response = await request(server)
+      .post('/')
+      .send({ query: 'mutation { userUpdate(id: 1, email: "bilbo@baggins.com") { id email } }' })
+      .set('Accept', 'application/json')
+      .expect(200)
+
+    expect(response.body.data.userUpdate.id).toEqual(1)
+    expect(response.body.data.userUpdate.email).toEqual('bilbo@baggins.com')
+  })
+
+  it("can update a user's shipping address", async () => {
+    const response = await request(server)
+      .post('/')
+      .send({ query: 'mutation { userUpdate(id: 1, email: "bilbo@baggins.com", address: "123 Fake Street") { id email address } }' })
+      .set('Accept', 'application/json')
+      .expect(200)
+
+    expect(response.body.data.userUpdate.id).toEqual(1)
+    expect(response.body.data.userUpdate.address).not.toEqual("456 Old Street")
+    expect(response.body.data.userUpdate.address).toEqual("123 Fake Street")
+  })
+
+  it("can update a user's image", async () => {
+    const response = await request(server)
+      .post('/')
+      .send({ query: 'mutation { userUpdate(id: 1, image: "image.jpg") {id image } }' })
+      .set('Accept', 'application/json')
+      .expect(200)
+
+    expect(response.body.data.userUpdate.id).toEqual(1)
+    expect(response.body.data.userUpdate.image).toEqual("image.jpg")
+  })
+
+  it("can update a user's description", async () => {
+    const response = await request(server)
+      .post('/')
+      .send({ query: 'mutation { userUpdate(id: 1, description: "I am the greatest of the admins") { id description } }'})
+      .expect(200)
+
+    expect(response.body.data.userUpdate.id).toEqual(1)
+    expect(response.body.data.userUpdate.description).toEqual("I am the greatest of the admins")
   })
 })
